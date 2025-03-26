ebbc92596c9bbf35f88b7b4eeb82ba0ff44299c3
Alsas75 <alex.minonis@gmail.com>
2025-03-26 20:50:33 +0100

    Test functionality implemented v. 2

--- src/main/java/app/Test2.java
+++ src/main/java/app/Test2.java
@@ -3,6 +3,6 @@ package app;
 public class Test2 {
     public static void main(String[] args) {
 
-        System.out.println("Это второй тестовый код");
+        System.out.println("Это второй тестовый код, версия 2");
     }
 }
